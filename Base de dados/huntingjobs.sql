3f084032a1b107b7dd825d0e9779122aee47d339
Tomás Jerónimo <2211892@my.ipleiria.pt>
2022-12-21 18:12:24 +0000

    Base de dados ExpProfissionais- continuação

--- Base de dados/huntingjobs.sql
+++ Base de dados/huntingjobs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 20-Dez-2022 às 19:07
+-- Tempo de geração: 21-Dez-2022 às 19:10
 -- Versão do servidor: 10.4.25-MariaDB
 -- versão do PHP: 8.0.23
 
